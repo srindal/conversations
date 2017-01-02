1d0598ea0e8fed202bf29ab2930911ee44e0e38f
srindal <rindal@gmail.com>
2017-01-02 22:53:01 +0100

    works with intervals

--- Program.cs
+++ Program.cs
@@ -1,62 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.RegularExpressions;
  
 namespace ConsoleApplication1
 {
     public class Program
-    {
- 
-        private static Predicate<object> f = o => (o.ToString().Equals("Bil"));
- 
- 
-        private static question[] f3 = { q("Det var ?rgerligt")};
- 
+    { 
+        private static question[] bil = 
+        {
+            q("I Alka håndterer vi over 30.000 bilskader hvert år. Vil du have nogle generelle råd?")
+                .OnYes("Jeg har ingen råd, men nice try"),
+            q("Inden vi starter skal vi lige tjekke de oplysninger vi har om dig"),
+            q("Er din bil en {carmodel}?")
+                .OnNo(
+                    q("Så er der noget galt med vores oplysninger. Indtast din bil", "carmodel"),
+                    q("Vi har nu registreret at din bil er en {carmodel}")),
+            q("Hvor og hvornår skete skaden?", "whereandwhen"),
+            q("Hvilke dele af bilen blev beskadiget?", "damage")
+                .On("fronten")
+                .On("bagsmækken")
+                .On("lygterne")
+                .On("venstre fordør"),
+            q("Hvor mange var I i bilen?", "passengers").OnInterval(1, 4),
+        q("Kære {name}. Vi har registreret at du {whereandwhen} har haft en skade på din {carmodel}, hvor I var {passengers} i bilen og {damage} blev beskadiget."),
+            q("Helmers autoværksted ringer til dig i morgen, så I kan aftale en tid")
+        };
+
         private static question[] f2 =
         {
             q("Hvad hedder du?", "name"),
-            q("Hej {name}"),
-            q("Hvad er g?et i stykker?", "skaden")
-                .On("Bil", f, f3)
-                .On("Hest", f, f3),
-            q("Det var kedelig med din {skaden} {name}")
+            q("Hej {name}, det er aldrig rart når man er ude for et uheld."),
+            q("Hvad er gået i stykker?")
+                .On("Bil", bil)
+                .On("Hest"),
+            q("Hav en fortsat god dag")
         };
- 
- 
-        /*private static question[] f1 =
-        {
-           qInt("Hvor hurtigt k?rte du?", i => i <= 130)
-                .OnNo(q("Du har k?rt for hurtigt. Det d?kker vi ikke")),
-            jn("K?rte du selv bilen?")
-                .OnNo(q("S? er du ikke d?kket"))
-                .OnYes(q("Der var du heldig"))
-        };
- 
-        static question[] flow = {
-            q("K?re John. Det er aldrig sjovt at f? en skade, men lad os sammen lave en skadesanmeldelse, s? klarer vi nemt og hurtigt papirarbejdet. Hvad er der sket?"),
-            jn("Vil du registrere noget med en bil").OnYes(f1),
-            qInt("Hvor gammel er du?", i => i >= 100).OnYes(q("Det tror jeg ikke p?")),
- 
-            jn("I Alka h?ndterer vi hvert ?r 30.000 bilskader. Vil du have r?d?")
-                .OnYes(q("hej med digh"))
-                .OnNo(
-                    q("Inden vi starter vil vi gerne lige tjekke at vi har de rigtige oplysninger om dig."),
-                    q("Tak for det Peter. Nu g?r vi igang med anmeldelsen. Hvilken bil har f?et skade?"),
-                    q("Hvor og hvorn?r skete skaden?"),
-                    jn("K?rte du selv bilen?")
-                        .OnNo(q("S? kan vi ikke hj?lpe dig")),
-                    q("Hvilke dele af bilen blev beskadiget?"))
-        };
- 
-*/
- 
+  
         public static void Main(string[] args)
         {
-            exec(f2.ToList(), new State());
+            var state = new State();
+            state.Set("carmodel", "Ford Mustang 1978");
+            exec(f2.ToList(), state);
         }
  
         public static void exec(List<question> flow, State st)
@@ -67,7 +51,7 @@ namespace ConsoleApplication1
                 do
                 {
                     var res = ask(q, st);
-                    accepted = q.preds.Count == 0 || q.preds.ContainsKey(res) && q.preds[res](res);
+                    accepted = q.isOk(res);
                     if (accepted && q.id != null)
                     {
                         st.Set(q.id, res);
@@ -75,8 +59,10 @@ namespace ConsoleApplication1
                     if (!accepted)
                     {
                         Console.WriteLine("!!!Du skal svare rigtigt!!!!");
+                    } else if (q.routes.Count > 0) {
+                        exec(q.routes[res].ToList(), st);                
                     }
-                } while (!accepted);
+                } while (!accepted);                
             }
         }
  
@@ -92,9 +78,9 @@ namespace ConsoleApplication1
  
         private static string ask(question q, State st)
         {
-            var s = q.q + (q.preds.Keys.Count > 0 ? "(" + String.Join("/", q.preds.Keys) + ")" : "");
+            var s = q.q + (q.preds.Keys.Count > 0 ? " (" + String.Join("/", q.preds.Keys) + ")" : "");
             Console.WriteLine(s.StringFormat(st.s));
-            return Console.ReadLine();
+            return q.id != null || q.preds.Count > 0 ? Console.ReadLine() : null;
         }
  
  
@@ -107,8 +93,8 @@ namespace ConsoleApplication1
  
         public string q { get; }
  
-        public Dictionary<object, question[]> routes = new Dictionary<object, question[]>();
-        public Dictionary<object, Predicate<object>> preds = new Dictionary<object, Predicate<object>>();
+        public Dictionary<string, question[]> routes = new Dictionary<string, question[]>();
+        public Dictionary<string, Predicate<object>> preds = new Dictionary<string, Predicate<object>>();
  
         public question(string q, string id)
         {
@@ -116,16 +102,36 @@ namespace ConsoleApplication1
             this.q = q;
         }
  
-        public question RegisterRoute(object key, question[] q)
+        public question RegisterRoute(string key, question[] q)
         {
-            routes.Add(key, q);
+            routes[key] = q;
             return this;
         }
  
-        public question RegisterPred(object key, Predicate<object> p)
+        public question RegisterPred(string key, Predicate<object> p)
         {
-            preds.Add(key, p);
+            preds[key] = p;
             return this;
+        }
+
+        public bool isOk(string key) {
+            if (preds.Count == 0) {
+                return true;
+            }
+            if (key == null) {
+                return false;
+            }
+            if (preds.ContainsKey(key)) {
+                return preds[key](key);
+            } else {
+                foreach (var p in preds) {
+                    if (preds[p.Key](key)) {
+                        return true;
+                    }
+                }
+            
+            }
+            return false;
         }
     }
  
@@ -151,18 +157,43 @@ namespace ConsoleApplication1
  
         public static question OnYes(this question jn, params question[] q)
         {
-            return On(jn, "Ja", s => !s.ToString().Equals("Nej"), q);
+            var res = On(jn, "Ja", q);
+            if (!jn.routes.ContainsKey("Nej")) {
+                jn.OnNo("");
+            }
+            return res;
         }
- 
+
+        public static question OnYes(this question q, string s) {
+            return OnYes(q, new question(s, null));
+        }
+
+        public static question OnNo(this question q, string s) {
+            return OnNo(q, new question(s, null));
+        }
+
+
         public static question OnNo(this question jn, params question[] q)
         {
-            return On(jn, "Nej", s => s.ToString().Equals("Nej"), q);
+            if (!jn.routes.ContainsKey("Ja")) {
+                jn.OnYes("");
+            }
+            return On(jn, "Nej", q);
         }
  
         public static question On(this question q, string key, Predicate<object> p, params question[] qs)
         {
             return q.RegisterRoute(key, qs).RegisterPred(key, p);
         }
+
+        public static question On(this question q, string key, params question[] qs) {
+            return On(q, key, s => s.Equals(key), qs);
+        }
+
+        public static question OnInterval(this question q, int from, int to, params question[] qs) {
+            return q.RegisterPred(from+"-"+to, s => Int32.Parse(s.ToString()) >= from && to >= Int32.Parse(s.ToString()));
+        }
+ 
  
         public static string StringFormat(this string format, IDictionary<string, string> values)
         {
